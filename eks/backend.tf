62a0b77419b138fa04674b8adc388607856efdb0
Basudev Jena <134388393+Basudevjena@users.noreply.github.com>
2025-06-20 20:19:41 +0530

    Update backend.tf
    Update the s3 name

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "bucket-is-mine-s3-three-tier-app"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
